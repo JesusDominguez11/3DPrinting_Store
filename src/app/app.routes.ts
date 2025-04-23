c0c31e1193307a454881a5bb11c30678a54ec94a
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-04-22 18:24:06 -0800

    reboot

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-
-import { CartComponent } from './components/cart/cart.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'cart', component: CartComponent } // Nueva ruta
-];

4baa3242c0a65f430430185b9aebac4faab8b3de
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-04-22 18:39:41 -0800

    page, components, services and models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,

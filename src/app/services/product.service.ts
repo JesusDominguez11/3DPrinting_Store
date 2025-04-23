9f2c7950cc2fd28e8a221b34dc5d5e666ade282a
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-04-23 03:21:22 -0800

    styles and product images

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,16 +6,31 @@ export class ProductService {
   private products: Product[] = [
     {
       id: '1',
-      name: 'Dragón 3D',
-      price: 15.99,
-      imageUrl: 'https://via.placeholder.com/150',
-      description: 'Figura detallada de dragón para colección'
+      name: 'Hatsune Miku',
+      price: 99.99,
+      image: 'assets/images/products/01_hatsune_miku.png',
+      description: 'Figura detallada de hatsune miku estilo funko.'
     },
     {
       id: '2',
-      name: 'Castillo medieval',
-      price: 22.50,
-      imageUrl: 'https://via.placeholder.com/150'
+      name: 'Happy Ghast',
+      price: 86.99,
+      image: 'assets/images/products/02_Ghast.png',
+      description: 'Figura de Happy Ghast de Minecraft.'
+    },
+    {
+      id: '3',
+      name: 'Pingu',
+      price: 84.99,
+      image: 'assets/images/products/03_pingu_perspective.jpg',
+      description: 'Figura de pingu.'
+    },
+    {
+      id: '4',
+      name: 'BMO',
+      price: 121.99,
+      image: 'assets/images/products/04_Adventure-Time-BMO-2.png',
+      description: 'Figura de BMO de hora de aventura.'
     }
   ];
 

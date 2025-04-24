e69621224aa03a5a107e94d754b164e4e2d1fcd1
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-04-23 21:35:42 -0700

    feature products bg removed

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,28 +8,28 @@ export class ProductService {
       id: '1',
       name: 'Hatsune Miku',
       price: 99.99,
-      image: 'assets/images/products/01_hatsune_miku.png',
+      image: 'assets/images/products/01_hatsune_miku-removebg-preview.png',
       description: 'Figura detallada de hatsune miku estilo funko.'
     },
     {
       id: '2',
       name: 'Happy Ghast',
       price: 86.99,
-      image: 'assets/images/products/02_Ghast.png',
+      image: 'assets/images/products/02_happy_ghast-removebg-preview.png',
       description: 'Figura de Happy Ghast de Minecraft.'
     },
     {
       id: '3',
       name: 'Pingu',
       price: 84.99,
-      image: 'assets/images/products/03_pingu_perspective.jpg',
+      image: 'assets/images/products/03_pingu-removebg-preview.png',
       description: 'Figura de pingu.'
     },
     {
       id: '4',
       name: 'BMO',
       price: 121.99,
-      image: 'assets/images/products/04_Adventure-Time-BMO-2.png',
+      image: 'assets/images/products/04_bmo-removebg-preview.png',
       description: 'Figura de BMO de hora de aventura.'
     }
   ];

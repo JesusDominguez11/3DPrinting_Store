94d1e6b8c95d533a7965b9cbcfb5c45e2c3776bf
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-04-30 13:22:35 -0700

    product model images convert to arrary

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
       id: '1',
       name: 'Hatsune Miku',
       price: 99.99,
-      image: 'assets/images/products/01_hatsune_miku-removebg-preview.png',
+      images: ['assets/images/products/01_hatsune_miku-removebg-preview.png'],
       description: 'Figura detallada de hatsune miku estilo funko.',
       category: 'anime',
       size: 'Mediano (15-20cm)'
@@ -17,7 +17,7 @@ export class ProductService {
       id: '2',
       name: 'Happy Ghast',
       price: 86.99,
-      image: 'assets/images/products/02_happy_ghast-removebg-preview.png',
+      images: ['assets/images/products/02_happy_ghast-removebg-preview.png'],
       description: 'Figura de Happy Ghast de Minecraft.',
       category: 'videogames',
       size: 'Grande (25-30cm)'
@@ -26,7 +26,7 @@ export class ProductService {
       id: '3',
       name: 'Pingu',
       price: 84.99,
-      image: 'assets/images/products/03_pingu-removebg-preview.png',
+      images: ['assets/images/products/03_pingu-removebg-preview.png'],
       description: 'Figura de pingu.',
       category: 'custom',
       size: 'Pequeño (5-10cm)'
@@ -35,7 +35,7 @@ export class ProductService {
       id: '4',
       name: 'BMO',
       price: 121.99,
-      image: 'assets/images/products/04_bmo-removebg-preview.png',
+      images: ['assets/images/products/04_bmo-removebg-preview.png'],
       description: 'Figura de BMO de hora de aventura.',
       category: 'custom',
       size: 'Mediano (15-20cm)'
